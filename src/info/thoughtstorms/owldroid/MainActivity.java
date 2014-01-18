766eb3886e754a69b6e6a9e249c00fd4d881fa73
phil jones <interstar@gmail.com>
2014-01-18 12:19:12 -0200

    removed dangerous 'branding' link from main menu bar

--- src/info/thoughtstorms/owldroid/MainActivity.java
+++ src/info/thoughtstorms/owldroid/MainActivity.java
@@ -61,7 +61,7 @@ public class MainActivity extends Activity {
 		
 	}
  
-	@Override 
+	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.activity_main, menu);
